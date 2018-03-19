d986410971e7f66ae5939291ffcd6835a7fb397f
Tuet Alexandre <tuet.alex@gmail.com>
2018-03-19 13:33:42 +0100

    Separation Header/Footer en fichiers externes

--- index.php
+++ index.php
@@ -1,68 +1,6 @@
-
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Pentagon &mdash; 100% Free Fully Responsive HTML5 Template by FREEHTML5.co</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FREEHTML5.CO" />
-
-  <!-- 
-	//////////////////////////////////////////////////////
-
-	FREE HTML5 TEMPLATE 
-	DESIGNED & DEVELOPED by FREEHTML5.CO
-		
-	Website: 		http://freehtml5.co/
-	Email: 			info@freehtml5.co
-	Twitter: 		http://twitter.com/fh5co
-	Facebook: 		https://www.facebook.com/fh5co
-
-	//////////////////////////////////////////////////////
-	 -->
-
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico">
-
-	<!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'> -->
-	
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<!-- Superfish -->
-	<link rel="stylesheet" href="css/superfish.css">
-
-	<link rel="stylesheet" href="css/style.css">
-
-
-	<!-- Modernizr JS -->
-	<script src="js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
-
-	</head>
+<?php
+	include('header.php');
+ ?>
 	<body>
 		<div id="fh5co-wrapper">
 		<div id="fh5co-page">
@@ -107,7 +45,7 @@
 										</li>
 										<li><a href="#">UI Animation</a></li>
 										<li><a href="#">Copywriting</a></li>
-										<li><a href="#">Photography</a></li> 
+										<li><a href="#">Photography</a></li>
 									</ul>
 								</li>
 								<li><a href="about.html">About</a></li>
@@ -118,9 +56,9 @@
 					</div>
 				</div>
 			</header>
-			
+
 		</div>
-		
+
 
 		<div class="fh5co-hero">
 			<div class="fh5co-overlay"></div>
@@ -216,11 +154,11 @@
 					</div>
 				</div>
 
-				
+
 			</div>
 		</div>
 
-		
+
 		<div id="fh5co-portfolio">
 			<div class="container">
 
@@ -231,7 +169,7 @@
 					</div>
 				</div>
 
-				
+
 				<div class="row row-bottom-padded-md">
 					<div class="col-md-12">
 						<ul id="fh5co-portfolio-list">
@@ -262,7 +200,7 @@
 								</a>
 							</li>
 
-							<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/work-2.jpg); "> 
+							<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/work-2.jpg); ">
 								<a href="#" class="color-5">
 									<div class="case-studies-summary">
 										<span>Web Design</span>
@@ -278,7 +216,7 @@
 									</div>
 								</a>
 							</li>
-						</ul>		
+						</ul>
 					</div>
 				</div>
 
@@ -288,12 +226,12 @@
 					</div>
 				</div>
 
-				
+
 			</div>
 		</div>
-		
 
-		
+
+
 		<div id="fh5co-content-section" class="fh5co-section-gray">
 			<div class="container">
 				<div class="row">
@@ -425,7 +363,7 @@
 									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 									<a href="#" class="btn btn-primary">Read More</a>
 								</div>
-							</div> 
+							</div>
 						</div>
 					</div>
 					<div class="col-lg-4 col-md-4">
@@ -439,7 +377,7 @@
 									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 									<a href="#" class="btn btn-primary">Read More</a>
 								</div>
-							</div> 
+							</div>
 						</div>
 					</div>
 					<div class="col-lg-4 col-md-4">
@@ -453,7 +391,7 @@
 									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 									<a href="#" class="btn btn-primary">Read More</a>
 								</div>
-							</div> 
+							</div>
 						</div>
 					</div>
 				</div>
@@ -466,52 +404,10 @@
 
 			</div>
 		</div>
-		<!-- fh5co-blog-section -->
-		<footer>
-			<div id="footer">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-6 col-md-offset-3 text-center">
-							<p class="fh5co-social-icons">
-								<a href="#"><i class="icon-twitter2"></i></a>
-								<a href="#"><i class="icon-facebook2"></i></a>
-								<a href="#"><i class="icon-instagram"></i></a>
-								<a href="#"><i class="icon-dribbble2"></i></a>
-								<a href="#"><i class="icon-youtube"></i></a>
-							</p>
-							<p>Copyright 2016 Free Html5 <a href="#">Pentagon</a>. All Rights Reserved. <br>Made with <i class="icon-heart3"></i> by <a href="http://freehtml5.co/" target="_blank">Freehtml5.co</a> / Demo Images: <a href="https://unsplash.com/" target="_blank">Unsplash</a></p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-	
 
-	</div>
-	<!-- END fh5co-page -->
-
-	</div>
-	<!-- END fh5co-wrapper -->
-
-	<!-- jQuery -->
-
-
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Stellar -->
-	<script src="js/jquery.stellar.min.js"></script>
-	<!-- Superfish -->
-	<script src="js/hoverIntent.js"></script>
-	<script src="js/superfish.js"></script>
-
-	<!-- Main JS -->
-	<script src="js/main.js"></script>
+<?php
+	include('footer.php');
+ ?>
 
 	</body>
 </html>
-
