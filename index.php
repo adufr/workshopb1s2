252d54bc5587e601666da2d9f67923a18032d930
AmQuin <34127277+AmQuin@users.noreply.github.com>
2018-03-20 15:22:41 +0100

    index

--- index.php
+++ index.php
@@ -2,12 +2,22 @@
 <?php
 	include('header.php');
 ?>
+<link rel="stylesheet" type="text/css" href="css/index.css"/>
+<body>
+	<link rel="stylesheet" type="text/css" href="index.css"/>
+	 <h1>Bienvenu sur BLIBLABOUM</h1>
+	<p>
+		Vous pouvez rechercher des forums,
+		discuter avec d'autres utilisateurs qui pourront vous aider sur des sujets,</br>
+ 		connaître leur disponibilité au sein de notre école ou en ligne,</br>
+ 		prendre connaissance de vos problèmes et essayer d'y résoudre.</br>
+	</p>
+	<a href="forum.php"><button type="button" name="button">
+		Forums
+	</button></a>
+</br>
 
-
-
-
-
-
+</body>
 
 	<?php
 		include('footer.php');
