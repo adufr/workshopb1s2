20fef465c6a357f45266e072a1f07efa9bacf426
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-03-20 17:08:45 +0100

    Ajout d'un vrai footer (reponsive)

--- footer.php
+++ footer.php
@@ -1,25 +1,55 @@
-<!-- fh5co-blog-section -->
-<footer>
-  <div id="footer">
-    <div class="container">
-      <div class="row">
+<link rel="stylesheet" type="text/css" href="css/footer.css"/>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 
-        <div class=".col-md-6 .offset-md-3 text-center">
-          <p>Copyright 2016 Free Html5 <a href="#">Pentagon</a>. All Rights Reserved. <br>Made with <i class="icon-heart3"></i> by <a href="http://freehtml5.co/" target="_blank">Freehtml5.co</a> / Demo Images: <a href="https://unsplash.com/" target="_blank">Unsplash</a></p>
-          <p>Etudiant à l'Epsi, nous avons créé ce site à la suite d'un projet National entre les différents campus Epsien.
-            Site créé par Ariana Montignac, Ambrine Quintana, Alexandre Tuet, Arthur Dufour </p>
+<footer class="footer-distributed">
 
-        <div class="col-md-6 col-md-offset-3 text-center">
-          <p class="fh5co-social-icons">
-            <a href="https://twitter.com/EPSI_Ecoles"><i class="icon-twitter2"></i></a>
-            <a href="https://en-gb.facebook.com/ecole.EPSI/"><i class="icon-facebook2"></i></a>
-            <a href="http://www.epsi.fr/"><i class="icon-dribbble2"></i></a>
-            <a href="https://www.youtube.com/user/ecoleepsi"><i class="icon-youtube"></i></a>
-          </p>
-        </div>
-      </div>
-    </div>
+  <div class="footer-left">
+    <p class="footer-links">
+      <a href="index.php">Accueil</a>
+      ·
+      <a href="forum.php">Forum</a>
+      ·
+      <a href="apropos.php">A propos</a>
+      ·
+      <a href="contact.php">Contact</a>
+    </p>
+    <p class="footer-company-name">EPSI &copy; 2018</p>
   </div>
+
+  <div class="footer-center">
+
+    <div>
+      <i class="fa fa-map-marker"></i>
+      <p><span>17 Rue du Sergent Michel Berthet</span> Lyon, France</p>
+    </div>
+
+    <div>
+      <i class="fa fa-phone"></i>
+      <p>+334 72 85 38 50</p>
+    </div>
+
+    <div>
+      <i class="fa fa-envelope"></i>
+      <p><a href="mailto:info@lyon-epsi.fr">info@lyon-epsi.fr</a></p>
+    </div>
+
+  </div>
+
+  <div class="footer-right">
+
+    <p class="footer-company-about">
+      <span>A propos de l'EPSI :</span>
+      Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+    </p>
+
+    <div class="footer-icons">
+      <a href="#"><i class="fa fa-facebook"></i></a>
+      <a href="#"><i class="fa fa-twitter"></i></a>
+      <a href="#"><i class="fa fa-linkedin"></i></a>
+    </div>
+
+  </div>
+
 </footer>
 
 
