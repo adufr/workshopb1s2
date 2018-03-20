71a3aea45f4db6def0848001aacf79c19f3bbdf5
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-03-20 19:25:36 +0100

    Modif nom du site

--- header.php
+++ header.php
@@ -70,7 +70,7 @@ if (isset($_POST['form_connexion'])) {
     <nav class="navbar navbar-inverse navbar-fixed-top" >
       <div class="container-fluid">
         <div class="navbar-header">
-          <a class="navbar-brand" href="index.php">Workshop</a>
+          <a class="navbar-brand" href="index.php">My hELPSI</a>
         </div>
         <ul class="nav navbar-nav">
           <li class="active"><a href="index.php">Accueil</a></li>
