399e7361756be48efabc7c57266a622247f24fc4
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-03-20 19:14:37 +0100

    Création session lors de l'inscription

--- inscription.php
+++ inscription.php
@@ -52,6 +52,36 @@ if (isset($_POST['form_inscription'])) {
 			                    $req_inser -> execute(array($uti_nom, $uti_prenom, $uti_mail, $uti_sexe, $uti_classe, $uti_campus, $uti_mdp));
 			                    $erreur = '';
 
+													// On attend 0.5 secondes :
+													sleep(0.5);
+
+
+													// On vérifie que le couple email / mot de passe existe dans la bdd :
+													$req_uti = $bdd -> prepare("SELECT * FROM UTILISATEUR WHERE uti_mail = ? AND uti_mdp = ?");
+													$req_uti -> execute(array($uti_mail, $uti_mdp));
+													$uti_existe = $req_uti -> rowCount();
+													if ($uti_existe == "1") {
+
+														// Tout est bon on initialise les variables de session :
+														$uti_infos = $req_uti -> fetch();
+														$_SESSION['uti_id'] = $uti_infos['uti_id'];
+														$_SESSION['uti_mail'] = $uti_infos['uti_mail'];
+														$_SESSION['uti_prenom'] = $uti_infos['uti_prenom'];
+														$_SESSION['uti_nom'] = $uti_infos['uti_nom'];
+														$_SESSION['uti_estadmin'] = $uti_infos['uti_estadmin'];
+														$_SESSION['uti_classe'] = $uti_infos['uti_classe'];
+														$_SESSION['uti_sexe'] = $uti_infos['uti_sexe'];
+														$_SESSION['uti_campus'] = $uti_infos['uti_campus'];
+														$_SESSION['uti_messages_envoyes'] = $uti_infos['uti_messages_envoyes'];
+														$_SESSION['uti_derniere_connexion'] = $uti_infos['uti_derniere_connexion'];
+
+														sleep(0.5);
+														header('Location: index.php');
+
+													} else {
+														$erreur = "Un problème est survenu";
+													}
+
 												} else {
 													$erreur = "Cette adresse mail est déjà utilisée";
 												}
