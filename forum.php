a87aa054c0bb42fa9b879accdb965427532d99a3
Ariana <ariana.aqifi@gmail.com>
2018-03-20 14:02:39 +0100

    forum.php

---
+++ forum.php
@@ -0,0 +1,363 @@
+<html class="no-js">
+<?php
+	include('header.php');
+?>
+
+
+<main id="fh5co-main" role="main">
+      <div class="container pb-lg">
+        <div class="row">
+
+<!-- 1e catégorie de forums, les langages de prog -->
+          <div class="col-md-12">
+            <div class="fh5co-heading text-center">
+                <h2 class="fh5co-heading-title">Langages de programmations</h2>
+              </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/ajax.png" alt="ajax.png">
+                </div>
+                <h2>Ajax</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/batch.png" alt="batch.png">
+                </div>
+                <h2>Batch</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/html_css.png" alt="html_css.png">
+                </div>
+                <h2>HTML & CSS</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/c-c++.png" alt="c-c++.png">
+                </div>
+                <h2>C & C++</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/c-sharp.png" alt="c-sharp.png">
+                </div>
+                <h2>C#</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/php.png" alt="php.png">
+                </div>
+                <h2>PHP</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/javascript.png" alt="js.png">
+                </div>
+                <h2>Javascript</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/perl.png" alt="perl.png">
+                </div>
+                <h2>Perl</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/python.png" alt="python.png">
+                </div>
+                <h2>Python</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/ruby.png" alt="ruby.png">
+                </div>
+                <h2>Ruby</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/swift.png" alt="swift.png">
+                </div>
+                <h2>Swift</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/symfony.png" alt="symfony.png">
+                </div>
+                <h2>Symfony</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/VisualBasic.png" alt="vb.png">
+                </div>
+                <h2>VisualBasic .NET</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+
+<!-- 2e catégorie : les frameworks -->
+          <div class="col-md-12">
+            <div class="fh5co-heading text-center">
+                <h2 class="fh5co-heading-title">frameworks</h2>
+              </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/php-framework.png" alt="php-fw.png">
+                </div>
+                <h2>PHP frameworks</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/react.png" alt="react.png">
+                </div>
+                <h2>Javascript frameworks</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/VisualBasic.png" alt="vb.png">
+                </div>
+                <h2>Java frameworks</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+
+<!-- 3e catégorie : Systèmes d'exploitation & mobile-->
+          <div class="col-md-12">
+            <div class="fh5co-heading text-center">
+              <h2 class="fh5co-heading-title">Systèmes d'exploitation & mobile</h2>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/linux.png" alt="linux.png">
+                </div>
+                <h2>Linux</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/windows.png" alt="windows.png">
+                </div>
+                <h2>Windows</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/appmobile.png" alt="appmobile.png">
+                </div>
+                <h2>Applications mobile</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+
+<!-- 4e catégorie : les cours généraux -->
+          <div class="col-md-12">
+            <div class="fh5co-heading text-center">
+                <h2 class="fh5co-heading-title">Général</h2>
+              </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/anglais.png" alt="anglais.png">
+                </div>
+                <h2>Anglais</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/francais.png" alt="fr.png">
+                </div>
+                <h2>Français</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/maths.png" alt="mayhs.png">
+                </div>
+                <h2>Mathématiques</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/algo.png" alt="algo.png">
+                </div>
+                <h2>Algorithme</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/reseau.png" alt="reseau.png">
+                </div>
+                <h2>Réseaux</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/management.png" alt="management.png">
+                </div>
+                <h2>Management</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xs-6 col-sm-6">
+            <div class="fh5co-main-service">
+              <a href="#" class="fh5co-block-links text-center">
+                <div class="icon-circle">
+                  <img class="logos" src="images/logos/economie.png" alt="economie.png">
+                </div>
+                <h2>Economie</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </a>
+            </div>
+          </div>
+
+
+
+
+        </div>
+      </div>
+    </main>
+
+<?php
+    include('footer.php');
+  ?>
