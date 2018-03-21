876baeb9bd3441cdc7af94644ffa5b8de807ad1a
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-03-22 00:17:05 +0100

    Ajout fontawesome

--- header.php
+++ header.php
@@ -49,7 +49,7 @@ if (isset($_POST['form_connexion'])) {
 
   <!--Importation des Polices-->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
