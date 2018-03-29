659f26f5a3e2c1d66001af05b3a5f22a2096eea9
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-03-29 08:33:33 +0200

    Suppression de l'utilisateur

--- bdd.php
+++ bdd.php
@@ -4,7 +4,7 @@ $debug = "true";
 if ($debug == "true") {
   $bdd = new PDO('mysql:host=localhost;dbname=workshopb1s2', 'root', '', array(\PDO::MYSQL_ATTR_INIT_COMMAND =>  'SET NAMES utf8') );
 } else {
-  $bdd = new PDO('mysql:host=localhost;dbname=workshopb1s2', 'workshop', 'workshop', array(\PDO::MYSQL_ATTR_INIT_COMMAND =>  'SET NAMES utf8') );
+  $bdd = new PDO('mysql:host=localhost;dbname=workshopb1s2', '****', '****', array(\PDO::MYSQL_ATTR_INIT_COMMAND =>  'SET NAMES utf8') );
 }
 
 ?>
